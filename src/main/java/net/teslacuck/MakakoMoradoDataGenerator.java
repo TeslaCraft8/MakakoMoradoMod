29f165c6fee0460a9ed751c57514e10893f69050
TeslaCraft8 <teslacraft87@gmail.com>
2025-10-27 18:28:31 +0000

    Initial commit

---
+++ src/main/java/net/teslacuck/MakakoMoradoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.teslacuck;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MakakoMoradoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
